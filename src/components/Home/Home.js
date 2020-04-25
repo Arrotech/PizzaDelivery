3767a5c5c79c9fdc5008977f669d05ba309e1e23
Arrotech <arrotechdesign@gmail.com>
2020-04-25 04:04:24 +0300

    Chore (Home Header Carousel)
    What does this pr do?
    
    Create a home header carousel that display a slideshow of images
    
    Any related pivotal stories?
    
    [Finishes #172512271]
    
    Screenshots

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,35 +1,92 @@
 import React, { Component } from "react";
-import { Navbar, Nav, Form, FormControl, NavDropdown, Button } from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  NavDropdown,
+  Button,
+  Carousel
+} from "react-bootstrap";
 
 export default class Home extends Component {
   render() {
     return (
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">Pizza</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">About</Nav.Link>
-            <NavDropdown title="Orders" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Order Status</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Update Destination
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Cancel Order</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Order Details
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#link">Contact</Nav.Link>
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Navbar>
+      <div className="home-header">
+        <Navbar sticky="top"  bg="light" expand="lg">
+          <Navbar.Brand href="#home">Pizza</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">About</Nav.Link>
+              <NavDropdown title="Orders" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">
+                  Order Status
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Update Destination
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Cancel Order
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Order Details
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#link">Contact</Nav.Link>
+            </Nav>
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+        <Carousel className="home-carousel">
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="pizza3.jpg"
+              alt="First slide"
+            />
+            <Carousel.Caption className="header-caption">
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="pizza2.jpg"
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="pizza.jpg"
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
     );
   }
 }
--- src/components/OrderCards/OrderCards.js
+++ src/components/OrderCards/OrderCards.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Container } from "react-bootstrap";
 import {
   MDBBtn,
   MDBContainer,
@@ -14,74 +15,76 @@ import {
 export default class OrderCards extends Component {
   render() {
     return (
-      <MDBContainer className="mdb-container">
-        <MDBRow>
-          <MDBCol>
-            <MDBCard wide>
-              <MDBCardImage cascade className="img-fluid" src="pizza.jpg" />
-              <MDBCardBody cascade>
-                <MDBCardTitle>Macaroni</MDBCardTitle>
-                <MDBCardText>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </MDBCardText>
-                <MDBCardText>$100</MDBCardText>
-                <MDBBtn className="order-btn" href="#">
-                  Add to cart
-                </MDBBtn>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBCol>
-          <MDBCol>
-            <MDBCard narrow>
-              <MDBCardImage cascade className="img-fluid" src="pizza.jpg" />
-              <MDBCardBody cascade>
-                <MDBCardTitle>Cheese</MDBCardTitle>
-                <MDBCardText>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </MDBCardText>
-                <MDBCardText>$62</MDBCardText>
-                <MDBBtn className="order-btn" href="#">
-                  Add to cart
-                </MDBBtn>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBCol>
-          <MDBCol>
-            <MDBCard cascade>
-              <MDBCardImage cascade className="img-fluid" src="pizza.jpg" />
-              <MDBCardBody cascade>
-                <MDBCardTitle>Butternut Squash</MDBCardTitle>
-                <MDBCardText>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </MDBCardText>
-                <MDBCardText>$55</MDBCardText>
-                <MDBBtn className="order-btn" href="#">
-                  Add to cart
-                </MDBBtn>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBCol>
-          <MDBCol>
-            <MDBCard cascade>
-              <MDBCardImage cascade className="img-fluid" src="pizza.jpg" />
-              <MDBCardBody cascade>
-                <MDBCardTitle>Crispy Sage</MDBCardTitle>
-                <MDBCardText>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </MDBCardText>
-                <MDBCardText>$55</MDBCardText>
-                <MDBBtn className="order-btn" href="#">
-                  Add to cart
-                </MDBBtn>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
+      <div className="pizza">
+        <MDBContainer className="mdb-container">
+          <MDBRow>
+            <MDBCol>
+              <MDBCard wide>
+                <MDBCardImage cascade className="img-fluid" src="pizza4.jpg" />
+                <MDBCardBody cascade>
+                  <MDBCardTitle>Macaroni</MDBCardTitle>
+                  <MDBCardText>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </MDBCardText>
+                  <MDBCardText>$100</MDBCardText>
+                  <MDBBtn className="order-btn" href="#">
+                    Add to cart
+                  </MDBBtn>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+            <MDBCol>
+              <MDBCard narrow>
+                <MDBCardImage cascade className="img-fluid" src="pizza5.jpg" />
+                <MDBCardBody cascade>
+                  <MDBCardTitle>Cheese</MDBCardTitle>
+                  <MDBCardText>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </MDBCardText>
+                  <MDBCardText>$62</MDBCardText>
+                  <MDBBtn className="order-btn" href="#">
+                    Add to cart
+                  </MDBBtn>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+            <MDBCol>
+              <MDBCard cascade>
+                <MDBCardImage cascade className="img-fluid" src="pizza6.jpg" />
+                <MDBCardBody cascade>
+                  <MDBCardTitle>Butternut Squash</MDBCardTitle>
+                  <MDBCardText>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </MDBCardText>
+                  <MDBCardText>$55</MDBCardText>
+                  <MDBBtn className="order-btn" href="#">
+                    Add to cart
+                  </MDBBtn>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+            <MDBCol>
+              <MDBCard cascade>
+                <MDBCardImage cascade className="img-fluid" src="pizza.jpg" />
+                <MDBCardBody cascade>
+                  <MDBCardTitle>Crispy Sage</MDBCardTitle>
+                  <MDBCardText>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </MDBCardText>
+                  <MDBCardText>$55</MDBCardText>
+                  <MDBBtn className="order-btn" href="#">
+                    Add to cart
+                  </MDBBtn>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </div>
     );
   }
 }
